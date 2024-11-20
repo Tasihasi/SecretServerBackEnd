aa17f9e4a71251eea8e57d0d63d317eca84055c9
Tasihasi <taskovics.dani12@gmail.com>
2024-11-20 17:47:54 +0100

    trying to inefrate posgress sql

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app/flask_app/app.db'  # Change this to point to the correct location
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE = os.path.join(os.getcwd(), 'app.db')  # Adjust path as needed
+    DATABASE = os.getenv("DATABASE_URL")  # Adjust path as needed
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
