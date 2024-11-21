7c42de29001a1df4644644d002722143c3396e8d
Tasihasi <taskovics.dani12@gmail.com>
2024-11-21 02:26:28 +0100

    cleaning code 2

--- app/flask_app/model/manage_db.py
+++ app/flask_app/model/manage_db.py
@@ -21,8 +21,6 @@ class ManageDB:
         except Exception as e:
             return False
 
-        
-
     @staticmethod
     def _check_retrievals() -> None:
         try: 
@@ -30,25 +28,22 @@ class ManageDB:
             SELECT hashText FROM secret WHERE retrievalCount <= 0;
             """)
 
-            # Execute the query and fetch all matching rows
             result = db.session.execute(select_query).fetchall()
 
-            # For each row, delete the corresponding record using the _delete_record method
             for row in result:
                 hash = row[0]
-                # Call the delete function for each hash
                 if not ManageDB._delete_record(hash):
                     return
 
         except Exception as e:
             return
 
-
     @staticmethod
     def _update_expiration_date() -> None:
 
         try: 
-            update_query = text( """
+            update_query = text
+            ( """
             UPDATE secret
             SET expiration = expiration - 1;
             """)
@@ -58,22 +53,18 @@ class ManageDB:
         except Exception as e:
             return
         
-
     @staticmethod
     def _delete_expired_data() -> None:
-
         try: 
-            select_query = text( """
+            select_query = text
+            ( """
             SELECT hashText FROM secret WHERE expiration <= 0;
             """)
 
-            # Execute the query and fetch all matching rows
             result = db.session.execute(select_query).fetchall()
 
-            # For each row, delete the corresponding record using the _delete_record method
             for row in result:
                 hash = row[0]
-                # Call the delete function for each hash
                 if not ManageDB._delete_record(hash):
                     return
 
