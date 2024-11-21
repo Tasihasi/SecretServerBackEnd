76c295a913838c9023f223feb7d543049579fad5
Tasihasi <taskovics.dani12@gmail.com>
2024-11-21 12:20:52 +0100

    gave up :(

--- app/main.py
+++ app/main.py
@@ -1,19 +1,12 @@
 from .flask_app import create_app, db
-from flask import Flask, jsonify
-import schedule, threading, time
+import schedule, time
 from .flask_app.model import ManageDB  # Import ManageDB here to avoid circular imports
 import logging
 
 app = create_app()
 
-# Configure the logging
-logging.basicConfig(level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
 # Initialize the scheduler after app creation
 def job():
-    raise RuntimeError ("Perhaps this will stop")
     ManageDB.ServerTick()
     
 
@@ -28,7 +21,6 @@ if __name__ == "__main__":
     schedule.every(1).minutes.do(job)
 
     while True:
-        raise RuntimeError ("meeeghs")
         schedule.run_pending()
         time.sleep(1)
 
