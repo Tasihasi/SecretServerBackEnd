b22ccbf351a2c5f156e1f0037e8197e8d5bc0d2a
Tasihasi <taskovics.dani12@gmail.com>
2024-11-21 12:01:01 +0100

    trying to run logging

--- app/main.py
+++ app/main.py
@@ -1,35 +1,20 @@
 from .flask_app import create_app, db
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 from flask import Flask, jsonify
-import requests
 import schedule, threading, time
 from .flask_app.model import ManageDB  # Import ManageDB here to avoid circular imports
+import logging
 
 app = create_app()
 
-def test_http():
-    url = 'https://secretserverbackend-production.up.railway.app/secret/shsuqis'  # Replace with the actual URL
+# Configure the logging
+logging.basicConfig(level=logging.INFO)
 
-    try:
-        # Make a GET request to the URL
-        response = requests.get(url)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Process the response if needed
-            data = response.json()  # or response.text if expecting plain text
-            return jsonify({'status': 'success', 'data': data}), 200
-        else:
-            return jsonify({'status': 'fail', 'message': f'Request failed with status code: {response.status_code}'}), response.status_code
-    
-    except requests.exceptions.RequestException as e:
-        # Handle any exceptions that occur during the request
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+logger = logging.getLogger(__name__)
 
 # Initialize the scheduler after app creation
 def job():
     ManageDB.ServerTick()
+    logging.info("Job R")
 
 def scheduler_thread():
     while True:
@@ -45,4 +30,3 @@ if __name__ == "__main__":
 
     app.run(debug=True)
     schedule.every(1).minutes.do(job)
-    schedule.every(1).minutes.do(test_http)
