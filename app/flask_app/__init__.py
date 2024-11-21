c10b7b190b34f8e3e9d62dc3ad7999df5c454afa
Tasihasi <taskovics.dani12@gmail.com>
2024-11-21 02:59:20 +0100

    removed the schedule from te __init__.py

--- app/flask_app/__init__.py
+++ app/flask_app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 from flask_sqlalchemy import SQLAlchemy
-#from . import db
 
 # TODO if the expire date is 0 then it never expires 
 # TODO implement the mimetype change
@@ -20,22 +17,6 @@ def create_app():
     db.init_app(app)
 
     CORS(app)
-
-    # Set up the scheduler to run the task every minute
-
-    from .model import ManageDB
-
-    #"""
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(
-        ManageDB.ServerTick(),  # This is the function you want to run
-        trigger=IntervalTrigger(minutes=1),  # The interval to run it
-        id='server_tick_job',  # Job ID (can be any unique string)
-        name='Run ServerTick every minute',  # Job name (optional)
-        replace_existing=True  # Replace the job if it already exists
-    )
-    #"""
-    scheduler.start()
 
     with app.app_context():
         from .controller.routes import main_blueprint
